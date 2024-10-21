169eb4172d1b7461b7ce3d6d61c7c96cf6953e6b
kevinramos2 <keramosl@unal.edu.co>
2024-10-21 16:19:31 -0500

    Empleados y UserPass

--- Mensajeria/Empleados.java
+++ Mensajeria/Empleados.java
@@ -1,5 +1,121 @@
 package Mensajeria;
 
-public class Empleados {
-  
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import Agenda.AgendaDoubleList;
+import Pack.Direccion;
+import Pack.Fecha;
+import Pack.Usuario;
+
+public class Empleados  {
+
+  public AgendaDoubleList empleados;
+
+    public Empleados(){
+      this.empleados = new AgendaDoubleList();
+    }
+
+   
+
+  	//import
+
+    public void importar() {
+        try (FileReader lector = new FileReader("Empleados.txt")) {
+        	BufferedReader leer = new BufferedReader(lector);
+            String linea;
+            while ((linea = leer.readLine()) != null) {
+            	String[] informacion = linea.split(" ");
+                
+                //settear los datos al objeto de tipo usuario
+            	Usuario nuevoUsuario = new Usuario();
+            	
+                nuevoUsuario.setNombre(informacion[0]);
+                nuevoUsuario.setId(cambiarStringInt(informacion[1]));
+
+
+                Fecha fecha1 = new Fecha(StringAShort(informacion[2]),StringAShort(informacion[3]),StringAShort(informacion[4]));
+                nuevoUsuario.setFecha_nacimiento(fecha1);
+                nuevoUsuario.setCiudad_nacimiento(informacion[5]);
+                nuevoUsuario.setTel(CambiarStringaLong(informacion[6]));
+                nuevoUsuario.setEmail(informacion[7]);
+
+
+                if (informacion[12] == null && informacion[13] == null){
+                  nuevoUsuario.setDir(Direccion.CambiarStringADir(direccionSinEdificio(informacion[8], informacion[9], informacion[10], informacion[11])));
+                }
+                else{
+                  nuevoUsuario.setDir(Direccion.CambiarStringADir(direccionConEdificio(informacion[8], informacion[9], informacion[10], informacion[11], informacion[12], informacion[13])));
+                }
+                
+                empleados.agregar(nuevoUsuario);
+                empleados.ordenarMenorAMayor();
+                
+            }
+           
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static short StringAShort(String shortinp) {
+      try {
+        short num = Short.parseShort(shortinp); 
+        return num;
+      }catch(Exception e) {
+        System.err.println("Error");
+        return 0;
+    }
+  }
+
+    public String direccionSinEdificio(String calle, String nomenclatura, String barrio, String ciudad){
+      Direccion dir = new Direccion(calle, nomenclatura, barrio, ciudad);
+      return dir.toString2();
+    }
+
+    public String direccionConEdificio(String calle, String nomenclatura, String barrio, String ciudad, String edificio, String apto){
+      Direccion dir = new Direccion(calle, nomenclatura, barrio, ciudad,edificio,apto);
+      return dir.toString1();
+    }
+
+    private long CambiarStringaLong(String h) {
+      try {
+        Long num = Long.parseLong(h); 
+        return num;
+      }catch(Exception e) {
+        System.err.println("Error");
+        return 0;
+      }
+    }
+
+    public int cambiarStringInt(String h) {
+      try {
+        int num = Integer.parseInt(h); 
+        return num;
+      }catch(Exception e) {
+        System.err.println("Error");
+        return 0;
+      }
+    }
+    
+
+
+
+    public void agregar(Usuario nuevoUsuario) {
+      empleados.agregar(nuevoUsuario);
+      empleados.ordenarMenorAMayor();
+      empleados.actualizarArchivoEmpleados();
+    }
+    
+    public void eliminar(int idEliminar) {
+        empleados.eliminar(idEliminar);
+        empleados.actualizarArchivoEmpleados();
+    }
+
+    public String nombre(int ID) {
+      return empleados.nombre(ID);
+    } 
 }
--- Mensajeria/UserPass.java
+++ Mensajeria/UserPass.java
@@ -1,5 +1,124 @@
 package Mensajeria;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import Agenda.AgendaDoubleList;
+import Pack.Usuario;
+
 public class UserPass {
-  
+
+  //atributos
+  public AgendaDoubleList usuariosContraseñas;
+  private int id;
+  private String password;
+  private String cargo;
+
+
+
+//constructor
+  public UserPass(){
+      this.usuariosContraseñas = new AgendaDoubleList();
+  }
+
+  public UserPass(int id, String password, String cargo){
+    this.usuariosContraseñas = new AgendaDoubleList();
+    this.id = id;
+    this.password = password;
+    this.cargo = cargo;
+  }
+
+
+  //gets y setts
+
+  public int getId(){
+    return id;
+  }
+  public void setId(int id){
+    this.id = id;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+  public void setPassword(String password){
+    this.password = password;
+  }
+
+  public String getCargo(){
+    return cargo;
+  }
+  public void setCargo(String cargo){
+    this.cargo = cargo;
+  }
+
+  	//import
+
+    public void importar() {
+        try (FileReader lector = new FileReader("Password.txt")) {
+        	BufferedReader leer = new BufferedReader(lector);
+            String linea;
+            while ((linea = leer.readLine()) != null) {
+            	String[] informacion = linea.split(" ");
+                
+              if (informacion.length == 3) {
+                
+            
+                //settear los datos al objeto de tipo usuario
+            	  Usuario nuevoUsuario = new Usuario(cambiarStringInt(informacion[0]),informacion[1],informacion[2]);
+    
+                usuariosContraseñas.agregar(nuevoUsuario);
+
+              }
+                
+            }             
+            
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    public void agregar(Usuario u){
+      usuariosContraseñas.agregar(u);
+      //Actualizar el archivo txt
+      usuariosContraseñas.actualizarArchivo();
+
+    }
+    public boolean esUser(int ID){
+      return usuariosContraseñas.buscarUser(ID) == 1;
+    }
+
+  public boolean esAdmin(int ID){
+    return usuariosContraseñas.buscarAdmin(ID) == 1;
+  }
+
+  public boolean claveCorrecta(String passWord) {
+    return usuariosContraseñas.PassCorrect(passWord) == 1;
+  }
+
+
+    private int cambiarStringInt(String h) {
+      try {
+        int num = Integer.parseInt(h); 
+        return num;
+      }catch(Exception e) {
+        System.err.println("Error");
+        return 0;
+      }
+    }
+
+    public void cambiarClave(int idCambio, String nuevaPass) {
+      usuariosContraseñas.CambiarContraseña(idCambio,nuevaPass);
+
+      //Actualizar el archivo txt
+      usuariosContraseñas.actualizarArchivo();
+    }
+
+    public void eliminar(int idEliminar) {
+      usuariosContraseñas.eliminar(idEliminar);
+      usuariosContraseñas.actualizarArchivo();
+    }
 }
